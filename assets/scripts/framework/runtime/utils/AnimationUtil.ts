1a8ffdbd4b1ef1761809906552c54356601423e6
kingBook <412126604@qq.com>
2024-09-21 17:44:01 +0800

    update 2024-09-21 17:43

---
+++ assets/scripts/framework/runtime/utils/AnimationUtil.ts
@@ -0,0 +1,26 @@
+﻿import { Animation, error } from "cc";
+
+export class AnimationUtil {
+
+    /**
+     * 播放动画到指定的sprite帧，并停止
+     * @param animation cc.Animation组件
+     * @param frameIndex 帧索引（大于等于0的整数）
+     * @param clipStateName 动画剪辑状态的名称
+     */
+    public static gotoAndStop(animation: Animation, frameIndex: number, clipStateName: string): void {
+        if (frameIndex === undefined || isNaN(frameIndex) || frameIndex < 0) {
+            error("frame 不能是 undefined、NaN、小于0");
+        }
+        frameIndex = frameIndex | 0; // 取整
+        const animState = animation.getState(clipStateName);
+        animState.stop();
+
+        const times: number[] = animState.clip['_tracks'][0]['_channel']['_curve']['_times'];
+        frameIndex = Math.min(frameIndex, times.length - 1);
+        const frameTime: number = times[frameIndex];
+        animState.setTime(frameTime);
+
+        animState.sample();
+    }
+}
--- assets/scripts/framework/runtime/utils/NodeUtil.ts
+++ assets/scripts/framework/runtime/utils/NodeUtil.ts
@@ -4,7 +4,7 @@ const { ccclass, property } = _decorator;
 /** 节点工具类 */
 //@ccclass('NodeUtil')
 export class NodeUtil extends Component {
-    
+
     /**
      * 在 bind 节点下创建一个新的节点，并挂载 t 组件
      * @param t 挂载的组件
@@ -17,6 +17,30 @@ export class NodeUtil extends Component {
         bind.addChild(node);
         let comp = node.addComponent(t);
         return comp;
+    }
+
+    public static getComponentInParent<T extends Component>(node: Node, componentType: new () => T): T | null {
+        let result = null;
+        let parent = node;
+        while (parent) {
+            result = parent.getComponent(componentType);
+            if (result) break;
+            parent = parent.parent;
+        }
+        return result;
+    }
+
+    public static getComponentsInParent<T extends Component>(node: Node, componentType: new () => T): T[] {
+        let result = new Array<T>();
+        let parent = node;
+        while (parent) {
+            let comps = parent.getComponents(componentType) as T[];
+            if (comps) {
+                result = result.concat(comps);
+            }
+            parent = parent.parent;
+        }
+        return result;
     }
 }
 
