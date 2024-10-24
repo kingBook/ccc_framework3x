59df76e47beedd5630e7a4414890e883973852fa
kingBook <412126604@qq.com>
2024-10-25 03:47:44 +0800

    update 2024-10-25 03:47

--- assets/scripts/app/Level.ts
+++ assets/scripts/app/Level.ts
@@ -6,12 +6,19 @@ const { ccclass, property } = _decorator;
 @ccclass('Level')
 export class Level extends Component {
 
+    private static s_instance: Level;
     private _fsm: LevelFsm;
-
+    
+    public static get instance(): Level { return Level.s_instance }
     public get fsm(): LevelFsm { return this._fsm; }
     
     protected onLoad(): void {
+        Level.s_instance = this;
         this._fsm = NodeUtil.addNodeComponent(LevelFsm, this.node);
+    }
+    
+    protected onDestroy(): void {
+        Level.s_instance = null;
     }
 
    
